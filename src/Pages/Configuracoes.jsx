de08049f2961cee15dbcbd4422c43d5fa73e1b86
marcos <marcosd3wow@gmail.com>
2021-06-14 18:24:29 -0300

    subindo requisito 3

---
+++ src/Pages/Configuracoes.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Config extends Component {
+  render() {
+    return (
+      <div>
+        <h1 data-testid="settings-title">CONFIGURAÇOESS</h1>
+      </div>
+    );
+  }
+}
