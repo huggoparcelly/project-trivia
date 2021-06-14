53bf642e45466642d6beace9419413fc99406af0
Renan0211 <renan.antunes.rocha@gmail.com>
2021-06-14 19:47:59 -0300

    Implementa o header

--- src/Pages/Game.jsx
+++ src/Pages/Game.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../Components/Header';
 
 export default class Game extends Component {
   render() {
     return (
       <div>
+        <Header />
         <h1>JOGO</h1>
       </div>
     );
