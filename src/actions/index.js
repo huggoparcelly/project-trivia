80dee7ec445916eddafa066c7f2c2dc3519d3a42
Renan0211 <renan.antunes.rocha@gmail.com>
2021-06-15 19:37:00 -0300

    Conserta erros de linter

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,3 @@
 export const login = (state) => ({ type: 'LOGIN', state });
 
-export const trivia = (state) => ({type: 'RECEIVE_TRIVIA', state});
+export const trivia = (state) => ({ type: 'RECEIVE_TRIVIA', state });
