417e016dfd419c04767f65523e5c05e9a5cfa858
Huggo Parcelly <h.parcelly@gmail.com>
2021-10-04 19:43:33 -0300

    Updates

---
+++ precache-manifest.73bfafe3b431361d83eb1f23b4784f30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e89c84d1ae52845bc983d036b78693",
+    "url": "/project-trivia/index.html"
+  },
+  {
+    "revision": "a67a69f1ac2bc196a2fa",
+    "url": "/project-trivia/static/css/main.98442190.chunk.css"
+  },
+  {
+    "revision": "27284d230ef93dbd8a5e",
+    "url": "/project-trivia/static/js/2.52f9b927.chunk.js"
+  },
+  {
+    "revision": "395cc4b3602716a6dc8075912d30f948",
+    "url": "/project-trivia/static/js/2.52f9b927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a67a69f1ac2bc196a2fa",
+    "url": "/project-trivia/static/js/main.62bb2300.chunk.js"
+  },
+  {
+    "revision": "d661d278f9eee2565a60",
+    "url": "/project-trivia/static/js/runtime-main.f937286e.js"
+  }
+]);
